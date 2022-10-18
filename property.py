4c896d97a75ff48bb7865e666dea84a334f6446d
Bohdan <myroshnyk.b@gmail.com>
2022-10-18 23:54:30 +0300

    CustomBtn() bind added.

--- property.py
+++ property.py
@@ -1,9 +1,11 @@
 from kivy.app import App
 from kivy.properties import ListProperty
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
+from kivy.uix.button import  Button
 
 
 class CustomBtn(Widget):
@@ -17,6 +19,20 @@ class CustomBtn(Widget):
 
     def on_pressed(self, instance, pos):
         print('pressed at {pos}'.format(pos=pos))
+
+
+class RootWidget(BoxLayout):
+
+    def __init__(self, **kwargs):
+        super(RootWidget, self).__init__(**kwargs)
+        self.add_widget(Button(text='btn 1'))
+        cb = CustomBtn()
+
+        def _local_func(instance, pos):
+            print('pos: printed from root widget: {pos}'.format(pos=pos))
+
+        cb.bind(pressed=_local_func)
+        self.add_widget(cb)
 
 
 class LoginScreen(GridLayout):
@@ -40,6 +56,10 @@ class LoginScreen(GridLayout):
         self.add_widget(Label(text='Custom button'))
         self.custombutton = CustomBtn()
         self.add_widget(self.custombutton)
+
+        self.add_widget(Label(text='Custom buttons 2'))
+        self.custombutton2 = RootWidget()
+        self.add_widget(self.custombutton2)
 
 
 class UserPage(App):
