7f085cdd3a5fde3d9948c033aff3a5e5af91bbf1
Bohdan <myroshnyk.b@gmail.com>
2022-10-18 22:30:22 +0300

    Scheduling a repetitive event file added.

---
+++ schedule.py
@@ -0,0 +1,39 @@
+import kivy
+
+kivy.require('2.1.0')  # replace with your current kivy version !
+
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.clock import Clock
+
+count = 0
+
+
+class MyApp(App):
+
+    def build(self):
+        return Label(text='Hello Twitch')
+
+    # def my_callback(dt):
+    #     print('My callback is called', dt)
+    #
+    # event = Clock.schedule_interval(my_callback, 1 / 30.)
+
+    # def my_callback(dt):
+    #     global count
+    #     count += 1
+    #     if count == 10:
+    #         print('Last call of my callback, bye bye !')
+    #         return False
+    #     print('My callback is called')
+    #
+    # Clock.schedule_interval(my_callback, 1 / 30.)
+
+    def my_callback(dt):
+        print("My callback is called!")
+
+    Clock.schedule_once(my_callback, 5)
+
+
+if __name__ == '__main__':
+    MyApp().run()
